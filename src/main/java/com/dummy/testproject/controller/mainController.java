658b0fad0a0197d6448a78a73e4c1f0e8337aae6
Bhashkar Chirag <bhaskarchirag7@gmail.com>
2023-03-23 10:35:17 +0530

    firstrsfft

--- src/main/java/com/dummy/testproject/controller/mainController.java
+++ src/main/java/com/dummy/testproject/controller/mainController.java
@@ -1,2 +1,16 @@
-package com.dummy.testproject.controller;public class mainController {
+package com.dummy.testproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+
+    @GetMapping("/test") // http://localhost:9000/test
+    public String test()
+    {
+        System.out.println("Inside about handler ....");
+        return "test";
+    }
 }
